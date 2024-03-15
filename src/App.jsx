039406b073735182bcc196322e38948bfb2a6730
ehf36 <ehf36@cornell.edu>
2024-03-15 18:25:41 -0400

    init

--- src/App.jsx
+++ src/App.jsx
@@ -14,18 +14,18 @@ function App() {
     <>
       <section className="h-screen w-[80%] flex mx-auto items-center">
         <div className="h-auto flex flex-row sm:flex-col-reverse space-x-10 sm:space-x-0 ">
-          <div className="flex flex-col justify-center space-y-5 text-base">
+          <div className="flex flex-col justify-center space-y-5 text-base w-full">
             {/* TODO 1: Edit the content of these tags!*/}
-            <h1>Hi! I'm ______</h1>
+            <h1>Hi! I'm Erin </h1>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Vestibulum ac ante eget arcu ultrices eleifend. Curabitur
-              tincidunt, quam non consequat cursus, nibh velit ultricies lorem,
-              et accumsan augue justo a diam.
+              Welcome to my portfolio :)
+              I am passionate about 'delightful' UX and Product Design
+              that inspires health and connectivity. I believe in empathy-centered design. 
+              
             </p>
             <div className="flex flex-row items-center space-x-2">
               <img src={location} />
-              <p>Ithaca, United States</p>
+              <p>Chicago, United States</p>
             </div>
             <div className="flex flex-row items-center">
               <div className="h-2 w-2 bg-green-500 rounded-full mr-2"></div>
@@ -34,7 +34,7 @@ function App() {
 
             {/* TODO 2: Change the links of these tags to link to your socials! */}
             <div className="flex flex-row h-10 space-x-2">
-              <a href="">
+              <a href="https://github.com/ehf36">
                 <img src={githublogo} />
               </a>
               <a href="">
